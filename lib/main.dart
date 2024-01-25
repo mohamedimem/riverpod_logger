6d6283d011442b16c44d2b602bb4fa79d79dc832
mohamedimem <imem.hamdi@etudiant-isi.utm.tn>
2024-01-25 23:38:50 +0100

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(home: Home());
-  }
+  }//
 }
 
 final counterProvider = StateProvider((ref) => 0, name: 'counter');
